dccba6cf0cf08f459e2385ce9905f720fc549793
RotaruX <rotarualex1612@gmail.com>
2025-02-03 13:35:19 +0100

    Commit inicial

---
+++ src/alexandruadrian_rotaru_cine/AlexandruAdrian_Rotaru_Cine.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package alexandruadrian_rotaru_cine;
+
+import java.util.Scanner;
+
+
+public class AlexandruAdrian_Rotaru_Cine {
+
+    Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Numero: ");
+        int a = sc.nextInt();
+        System.out.println(a);
+    }
+    
+}
