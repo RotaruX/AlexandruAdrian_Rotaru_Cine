f37e71dd12dc94a31a39fac2d39eccb8c0c07b2a
RotaruX <rotarualex1612@gmail.com>
2025-02-13 12:40:46 +0100

    salas acabado

--- src/alexandruadrian_rotaru_cine/AlexandruAdrian_Rotaru_Cine.java
+++ src/alexandruadrian_rotaru_cine/AlexandruAdrian_Rotaru_Cine.java
@@ -15,79 +15,69 @@ public class AlexandruAdrian_Rotaru_Cine {
     static LocalDate fecha = LocalDate.now();
     static DateTimeFormatter ESP = DateTimeFormatter.ofPattern("EEEE d 'de' MMMM 'de' yyyy", new Locale("es", "ES"));
     static String fechaActual = fecha.format(ESP);
-    static char[][] sala1;
-    static char[][] sala2;
-    static String pelicula1, pelicula2;
-    static int mitadSala1, mitadSala2;
+    static char[][][] cine;
+    static String[] peliculas;
 
     public static void main(String[] args) {
         System.out.println("Configuracion de las salas para hoy");
         Fecha();
-        for (int i = 1; i <= 2; i++) {
-            Teclado(i);
-        }
-
-        Rellenar();
+        Salas();
         Menu();
     }
 
-    //Muestra la fecha de hoy
     public static void Fecha() {
-
         System.out.println(fechaActual);
         System.out.println("Sesión 22:30");
         System.out.println();
     }
 
-    //Pedir por teclado
-    public static void Teclado(int i) {
-        int filas, butacas;
-        System.out.println("Sala " + i);
+    public static void Salas() {
+        int salas, filas, butacas;
         do {
-            System.out.print("Número de filas: -> ");
-            filas = sc.nextInt();
-            if (filas <= 0){
-                System.out.println("Debe haber minimo 1 fila");
+            System.out.print("Introduce numero de salas (maximo 10): ");
+            salas = sc.nextInt();
+            if (salas < 0 || salas > 10) {
+                System.out.println("Maximo 1o salas");
             }
-        } while (filas <= 0);
-        do {
-            System.out.print("Butacas por filas: -> ");
-            butacas = sc.nextInt();
-            if (butacas <= 1 || butacas % 2 != 0){
-                System.out.println("Debe haber minimo 2 butacas y debe ser par ");
-            }
-        } while (butacas <= 2 || butacas % 2 != 0);
-        sc.nextLine();
-        if (i == 1) {
-            System.out.print("Pelicula de la Sala 1: ");
-            pelicula1 = sc.nextLine();
-            mitadSala1 = butacas / 2 + 1;
-            sala1 = new char[filas][butacas];
-        } else {
-            System.out.print("Pelicula de la Sala 2: ");
-            pelicula2 = sc.nextLine();
-            mitadSala2 = butacas / 2 + 1;
-            sala2 = new char[filas][butacas];
+        } while (salas < 0 || salas > 10);
+
+        //Colocamos el numero de salas y asignamos las filas y las butacas
+        cine = new char[salas][][];
+        peliculas = new String[salas];
+        for (int i = 0; i < cine.length; i++) {
+            do {
+                System.out.print("Número de filas " + "de la sala " + (i + 1) + ": -> ");
+                filas = sc.nextInt();
+                if (filas <= 0) {
+                    System.out.println("Debe haber minimo 1 fila");
+                }
+            } while (filas <= 0);
+            do {
+                System.out.print("Butacas por filas: -> ");
+                butacas = sc.nextInt();
+                if (butacas <= 1 || butacas % 2 != 0) {
+                    System.out.println("Debe haber minimo 2 butacas y debe ser par ");
+                }
+            } while (butacas < 2 || butacas % 2 != 0);
+            cine[i] = new char[filas][butacas];
+            sc.nextLine();
+            System.out.print("Introduce pelicula para la sala " + (i + 1) + " : ");
+            peliculas[i] = sc.nextLine();
+            System.out.println();
         }
-        System.out.println();
+        Rellenar();
     }
 
-    //Rellena los arrays
     public static void Rellenar() {
-        for (int i = 0; i < sala1.length; i++) {
-            for (int j = 0; j < sala1[i].length; j++) { // Cambiado i++ por j++
-                sala1[i][j] = '_'; // Pasillo central
-            }
-        }
-
-        for (int i = 0; i < sala2.length; i++) {
-            for (int j = 0; j < sala2[i].length; j++) { // Cambiado i++ por j++
-                    sala2[i][j] = '_';
+        for (int i = 0; i < cine.length; i++) {
+            for (int j = 0; j < cine[i].length; j++) {
+                for (int k = 0; k < cine[i][j].length; k++) {
+                    cine[i][j][k] = '_';
+                }
             }
         }
     }
 
-    // Menú con las diferentes opciones
     public static void Menu() {
         int opcion;
         do {
@@ -100,169 +90,132 @@ public class AlexandruAdrian_Rotaru_Cine {
             } while (opcion != 1 && opcion != 2 && opcion != 0);
 
             switch (opcion) {
-                case 1:
-                    MostrarSala1();
-                    MostrarSala2();
-                    break;
-                case 2:
+                case 1 ->
+                    Mostrar();
+                case 2 ->
                     Vender();
-                    break;
-                case 0:
+                case 0 ->
                     System.out.println("Final del programa");
-                    break;
             }
         } while (opcion != 0);
     }
 
-    public static void MostrarSala1() {
-        System.out.println("SALA 1 -> " + pelicula1);
-        for (int i = 0; i < sala1.length; i++) {
-            // Imprimir número de fila
-            System.out.printf("%2d ", i + 1);
-            for (int j = 0; j < sala1[i].length; j++) {
-                if (j == mitadSala1 - 1) {
+    public static void Mostrar() {
+        for (int i = 0; i < cine.length; i++) {
+            System.out.println("Sala " + (i + 1) + " -> " + peliculas[i]);
+            for (int j = 0; j < cine[i].length; j++) {
+                System.out.printf("%2d ", j + 1);
+                for (int k = 0; k < cine[i][j].length; k++) {
+                    if (k == cine[i][j].length / 2) {
+                        System.out.print(" ");
+                    }
+                    System.out.print(cine[i][j][k] + " ");
+                }
+                System.out.println();
+            }
+            //Escribir unidades
+            System.out.print("   ");
+            for (int ud = 1; ud <= cine[i][0].length; ud++) {
+                System.out.print(ud % 10 + " ");
+                if (ud == cine[i][0].length / 2) {
                     System.out.print(" ");
                 }
-                System.out.print(sala1[i][j] + " ");
             }
+            //Escribir decenas
             System.out.println();
-        }
-
-        // Imprimir números de butacas
-        System.out.print("   ");
-        for (int j = 1; j <= sala1[0].length; j++) {
-            if (j == mitadSala1) {
-                System.out.print(" ");
-            }
-            System.out.print(j % 10+ " ");
-        }
-
-        //imprimir decenas
-        System.out.println();
-        System.out.print("   ");
-        for (int j = 1; j <= sala1[0].length; j++) {
-            if (j == mitadSala1) {
-                System.out.print(" ");
-            }
-            if (j >= 10 && j % 10 == 0) {
-                System.out.print(j / 10 + " ");
-            } else {
-                System.out.print("  ");
-            }
-        }
-        System.out.println();
-    }
-
-    // Opción mostrar salas
-    public static void MostrarSala2() {
-        // Mostrar sala 2
-        System.out.println("\n SALA 2 -> " + pelicula2);
-        for (int i = 0; i < sala2.length; i++) {
-            // Imprimir número de fila
-            System.out.printf("%2d ", i + 1);
-            for (int j = 0; j < sala2[i].length; j++) {
-                if (j == mitadSala2 - 1) {
+            System.out.print("   ");
+            for (int dec = 1; dec <= cine[i][0].length; dec++) {
+                if (dec >= 10 && dec % 10 == 0) {
+                    System.out.print(dec / 10 + " ");
+                }else{
+                    System.out.print("  ");
+                }
+                if (dec == cine[i][0].length / 2) {
                     System.out.print(" ");
                 }
-                System.out.print(sala2[i][j] + " ");
             }
             System.out.println();
+            System.out.println();
         }
-
-        // Imprimir números de butacas
-        System.out.print("   ");
-        for (int j = 1; j <= sala2[0].length; j++) {
-            if (j == mitadSala2) {
-                System.out.print(" ");
-            }
-            System.out.print(j % 10 + " ");
-        }
-
-        //imprimir decenas
-        System.out.println();
-        System.out.print("   ");
-        for (int j = 1; j <= sala2[0].length; j++) {
-            if (j == mitadSala2) {
-                System.out.print(" ");
-            }
-            if (j >= 10 && j % 10 == 0) {
-                System.out.print(j / 10 + " ");
-            } else {
-                System.out.print("  ");
-            }
-        }
-        System.out.println();
-        System.out.println();
     }
 
-    //Venta de entrada
     public static void Vender() {
         int sala, fila, butaca;
         System.out.println("Venta de entradas");
-        do {
+        do{
             System.out.print("Introduce sala: ");
             sala = sc.nextInt();
-            if (sala > 2 || sala < 1){
-                System.out.println("Sala inexistente");
+            if(sala < 1 || sala > cine.length){
+                System.out.println("Esta sala no existe");
             }
-        } while (sala > 2 || sala < 1);
-
-        if (sala == 1) {
-            MostrarSala1();
-            do {
-                System.out.print("Fila-> ");
-                fila = sc.nextInt();
-                if (fila > sala1.length){
-                    System.out.println("Hay " + sala1.length + " filas");
-                }
-            } while (fila > sala1.length);
-            fila--;
-            do {
-                System.out.print("Butaca-> ");
-                butaca = sc.nextInt();
-                if (butaca > sala1[fila].length){
-                    System.out.println("Hay " + sala1[fila].length + " butacas");
-                }
-            } while (butaca > sala1[fila].length);
-            butaca--;
-
-            if (sala1[fila][butaca] != 'X') {
-                sala1[fila][butaca] = 'X';
-                System.out.println();
-                System.out.println();
-                System.out.println("Sala 1 " + pelicula1);
-                System.out.println(fechaActual + " Sesión 22:30");
-                System.out.println("Fila " + (fila + 1) + " Butaca " + (butaca + 1));
-                System.out.println();
-            } else {
-                System.out.println("Butaca ocupada");
+        }while(sala < 1 || sala > cine.length);
+        
+        sala -= 1;
+        System.out.println();
+        System.out.println("Sala " + (sala + 1) + " -> " + peliculas[sala]);
+        for(int i = 0; i < cine[sala].length; i++){
+            System.out.printf("%2d ", i + 1);
+            for(int j = 0; j < cine[sala][i].length; j++){
+                if (j == cine[sala][i].length / 2) {
+                        System.out.print(" ");
+                    }
+                    System.out.print(cine[sala][i][j] + " ");
             }
-        } else {
-            MostrarSala2();
-            //Pedimos fila y butaca
-            do {
-                System.out.print("Fila-> ");
-                fila = sc.nextInt();
-            } while (fila > sala2.length);
-            fila--;
-            do {
-                System.out.print("Butaca-> ");
-                butaca = sc.nextInt();
-            } while (butaca > sala2[fila].length);
-            butaca--;
-            
-            //Comprobamos y cambiamos el contenido del array
-            if (sala2[fila][butaca] != 'X') {
-                sala2[fila][butaca] = 'X';
-                System.out.println();
-                System.out.println();
-                System.out.println("Sala 2 " + pelicula2);
-                System.out.println(fechaActual + " Sesión 22:30");
-                System.out.println("Fila " + (fila + 1) + " Butaca " + (butaca + 1));
-                System.out.println();
-            } else {
-                System.out.println("Butaca ocupada");
-            }
+            System.out.println();
         }
+        
+        System.out.print("   ");
+            for (int ud = 1; ud <= cine[sala][0].length; ud++) {
+                System.out.print(ud % 10 + " ");
+                if (ud == cine[sala][0].length / 2) {
+                    System.out.print(" ");
+                }
+            }
+            //Escribir decenas
+            System.out.println();
+            System.out.print("   ");
+            for (int dec = 1; dec <= cine[sala][0].length; dec++) {
+                if (dec >= 10 && dec % 10 == 0) {
+                    System.out.print(dec / 10 + " ");
+                }else{
+                    System.out.print("  ");
+                }
+                if (dec == cine[sala][0].length / 2) {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+            
+            do{
+                System.out.print("Fila -> ");
+                fila = sc.nextInt();
+                if(fila < 1 || fila > cine[sala].length){
+                    System.out.println("Fila no válida.");
+                }
+            }while(fila < 1 || fila > cine[sala].length);
+            
+            fila -= 1;
+            
+            do{
+                System.out.print("Butaca -> ");
+                butaca = sc.nextInt();
+                if(butaca < 1 || butaca > cine[sala][fila].length){
+                    System.out.println("Butaca no válida.");
+                }
+            }while(butaca < 1 || butaca > cine[sala][fila].length);
+            
+            butaca -= 1;
+            
+             if (cine[sala][fila][butaca] != 'X') {
+                cine[sala][fila][butaca] = 'X';
+                System.out.println();
+                System.out.println();
+                System.out.println("Sala  " + (sala + 1)+ " " + peliculas[sala]);
+                System.out.println(fechaActual + " Sesión 22:30");
+                System.out.println("Fila " + (fila + 1) + " Butaca " + (butaca + 1));
+                System.out.println();
+            } else {
+                System.out.println("Butaca ocupada");
+            }
     }
 }
